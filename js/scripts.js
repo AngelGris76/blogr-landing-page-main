3709bf698ca35ad0a1eba7d5830090e839c65db4
AngelGris76 <sebastian.smuraglia@gmail.com>
2021-04-03 10:53:08 -0300

    funcionalidad mobile terminada

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+const mainMenu = document.getElementById('main-menu');
+const menuContainer = document.getElementById('menu-container');
+const subMenus = document.getElementsByClassName('main-submenu');
+
+const isSubMenu = (element) =>
+{
+    if ((element.tagName === 'A') && (element.nextElementSibling.tagName === 'UL')) return true;
+    else
+        return false;
+}
+
+mainMenu.addEventListener('click', (e) =>
+{
+    if (e.target.tagName === 'NAV') e.target.children[0].classList.toggle('menu-container--show');
+    if (isSubMenu(e.target))
+    {
+        e.target.nextElementSibling.classList.toggle('hide-submenu');
+        e.target.classList.toggle('main-menu__link--expanded')
+    }
+});
+
+menuContainer.addEventListener('transitionend', (e) =>
+{
+    e.target.parentElement.classList.toggle('main-menu--show');
+    e.target.parentElement.parentElement.parentElement.classList.toggle('intro--show');
+    if (!mainMenu.classList.contains('main-menu-show'))
+    {
+        for (const subMenu of subMenus) 
+        {
+            subMenu.classList.add('hide-submenu')
+            subMenu.previousElementSibling.classList.remove('main-menu__link--expanded')
+        }
+    }
+});
